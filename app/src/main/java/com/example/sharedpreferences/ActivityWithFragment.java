1da5fe9b9d0ea50ecede3130fe808311c1d7c3b8
anggy70 <anggydana@gmail.com>
2020-11-28 12:45:42 +0700

    Ini Bebas....

---
+++ app/src/main/java/com/example/sharedpreferences/ActivityWithFragment.java
@@ -0,0 +1,14 @@
+package com.example.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityWithFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_with_fragment);
+    }
+}
